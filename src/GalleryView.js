9ed5dbc7cf959362f509ac39d883f91ae3bf0164
Arinana <allyut12zk@gmail.com>
2026-02-10 15:21:08 +0300

    Update GalleryView.js

--- src/GalleryView.js
+++ src/GalleryView.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { playSong, generateReview, generateCover } from "./utils";
+import { playSong, generateCover } from "./utils";
 import "@fortawesome/fontawesome-free/css/all.min.css"; // иконки Font Awesome
 
 const API_URL = process.env.REACT_APP_API_URL + "/songs";
